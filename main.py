cf106dc8a4a7e0af7fa6a1f82d053305e2d614f6
Kevin C. Tofel <kevin@kctofel.com>
2024-10-26 16:19:54 -0400

    Added 60fps support

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
+
+    clock = pygame.time.Clock()
+    dt = 0
+    ("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,6 +17,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
